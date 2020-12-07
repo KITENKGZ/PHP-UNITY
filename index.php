5c24cbebfbcf4b76701fdff692d48e2e867272cd
antinooboos11 <56489448+antinooboos11@users.noreply.github.com>
2020-12-07 19:35:06 +0600

    .dota

--- index.php
+++ index.php
@@ -155,9 +155,16 @@
               </a>
             <?php endforeach; ?>
           </div>
-          <div id="ESPORT" class="none">
-               <div class="game__block"> <a href="#" > <img src="" alt=""> </a> </div>
-          </div>
+          <div  id="ESPORT" class="ESPORT">
+            <div class="sport__inner">
+               <div class="game__block"> <a href="#" > <img src="img/stream/csgo.png" alt="csgo"> </a> </div>
+               <div class="game__block"> <a href="#" > <img src="img/stream/dota2.png" alt="dota"> </a> </div>
+               <div class="game__block"> <a href="#" > <img src="img/stream/codm.png" alt="cod"> </a> </div>
+               <div class="game__block"> <a href="#" > <img src="img/stream/pubg_m.png" alt="pubg"> </a> </div>
+               <div class="game__block"> <a href="#" > <img src="img/stream/mlbb.png" alt="ml"> </a> </div>
+               <div class="game__block"> <a href="#" > <img src="img/stream/lol.png" alt="lol"> </a> </div>
+              </div>
+            </div>
           <div id="FIFA" class="none">
           <?php foreach($streamfifa as $stream): ?>
               <a style="text-decoration: none;" href="stream.php?stream_id=<?=$stream->id;?>">
